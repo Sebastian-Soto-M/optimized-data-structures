8aa0f972f1b96d489e9ecb4d8250eb8a511a826d
Sebastian Soto Madrigal <ssotom@ucenfotec.ac.cr>
2021-01-28 13:09:24 -0600

    feat: basic navigation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clean-template'`, () => {
+  it(`should have as title 'demo-structures'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clean-template');
+    expect(app.title).toEqual('demo-structures');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('clean-template app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('demo-structures app is running!');
   });
 });
--- src/app/core/layout/navigation/navigation.component.ts
+++ src/app/core/layout/navigation/navigation.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
+  title = 'data structures';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
